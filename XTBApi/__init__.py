e33af723521fac3c1f76d1d7ce66a3b2769eb76f
Clément PAPPALARDO <16377344+lemassykoi@users.noreply.github.com>
2021-01-26 05:19:05 +0100

    Update __init__.py

--- XTBApi/__init__.py
+++ XTBApi/__init__.py
@@ -8,7 +8,7 @@ logging.config.dictConfig({
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
-        'default': {
+        'deafult': {
             'format':
                 '%(asctime)s - %(levelname)s - %(name)s - %(message)s',
             'datefmt': '%Y-%m-%d %H:%M:%S'
@@ -17,11 +17,11 @@ logging.config.dictConfig({
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
-            'formatter': 'default',
+            'formatter': 'deafult',
         },
         'rotating': {
             'class': 'logging.handlers.TimedRotatingFileHandler',
-            'formatter': 'default',
+            'formatter': 'deafult',
             'filename': os.path.join(os.path.dirname(__file__), 'logs/' + str(date.today()) + '.log'),
             'when': 'midnight',
             'backupCount': 3
